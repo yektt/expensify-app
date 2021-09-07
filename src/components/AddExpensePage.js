dc0b8405aea59c3a952ef2b248defcf4cd81f636
yektt <yektaturan.93@gmail.com>
2021-09-07 11:43:56 +0200

    AppRouter has been broken into smaller components

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AddExpensePage = () => (
+  <div>
+    This is from add expense component.
+  </div>
+);
+
+export default AddExpensePage;
---
+++ src/components/HelpPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HelpPage = () => (
+  <div>
+    This is from help component.
+  </div>
+);
+
+export default HelpPage;
