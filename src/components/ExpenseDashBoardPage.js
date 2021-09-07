417a39d8db3a5d7bd457019e29f80cc207ac49f0
yektt <yektaturan.93@gmail.com>
2021-09-07 11:49:25 +0200

    Fixed the components

---
+++ src/components/ExpenseDashBoardPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from dashboard component.
+  </div>
+);
+
+export default ExpenseDashboardPage;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,16 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Switch} from "react-router-dom";
 import Header from "../components/Header";
+import ExpenseDashboardPage from "../components/ExpenseDashBoardPage";
 import AddExpensePage from "../components/AddExpensePage";
 import EditExpensePage from "../components/EditExpensePage";
 import HelpPage from "../components/HelpPage";
 import NotFoundPage from "../components/NotFoundPage";
-
-const ExpenseDashboardPage = () => (
-  <div>
-    This is from dashboard component.
-  </div>
-);
 
 const AppRouter = () => (
   <BrowserRouter>
