884e12cff98fb8242f0ce7eaf7c8ce6b65028114
yektt <yektaturan.93@gmail.com>
2021-09-08 11:59:10 +0200

    Passed dynamic values to the store

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,12 +3,14 @@ import {createStore} from 'redux';
 const store = createStore((state = { count: 0}, action) => {
   switch (action.type) {
     case 'INCREMENT': 
+    const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
       };
     case 'RESET':
       return {
@@ -19,15 +21,38 @@ const store = createStore((state = { count: 0}, action) => {
   }
 });
 
+// // with store.subscribe, we can watch over the store
+// // whenever there is a change, it will be called.
+// store.subscribe(() => {
+//   console.log(store.getState());
+// });
+
+// if we want to stop to watching store, 
+// assign the subscribe function to a variable
+// when you want to stop it, you need to call the variable as a function.
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
 // Actions - nothing more than an object that gets sent to the store
 // Actions allow us to change the store state.
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'INCREMENT',
+  incrementBy: 5
 });
 
 store.dispatch({
   type: 'DECREMENT'
 });
+
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+});
+
+// // if you will activate this code,
+// // the subscribe will stop
+// unsubscribe();
 
 store.dispatch({
   type: 'DECREMENT'
@@ -36,5 +61,3 @@ store.dispatch({
 store.dispatch({
   type: 'RESET'
 });
-
-console.log(store.getState());
