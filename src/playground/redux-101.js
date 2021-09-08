04f2baba21a921f3a16d80328ea09d4c7c7cb8eb
yektt <yektaturan.93@gmail.com>
2021-09-08 18:19:43 +0200

    Used action generator for decreasing, setting and resetting the count

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -13,10 +13,25 @@ import {createStore} from 'redux';
 // because if we will only write payload there,
 // when incrementCount will be called without parameters,
 // it will throw an error (it will try to find payload.incrementBy)
-const incrementCount = ( { incrementBy = 1}) => ({
+const incrementCount = ({ incrementBy = 1} = {}) => ({
   type: 'INCREMENT',
-  incrementBy: incrementBy // or only incrementBy will be enough
+  incrementBy 
 });
+
+const decrementBy = ({decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
+const resetCount = () => ({
+  type: 'RESET'
+});
+
+const setCount = ({count}) => ({
+  type: 'SET',
+  count
+});
+
 
 const store = createStore((state = { count: 0}, action) => {
   switch (action.type) {
@@ -25,9 +40,8 @@ const store = createStore((state = { count: 0}, action) => {
         count: state.count + action.incrementBy
       };
     case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       };
     case 'SET':
       return {
@@ -42,48 +56,40 @@ const store = createStore((state = { count: 0}, action) => {
   }
 });
 
-store.dispatch({
-  type: 'SET',
-  count: 101
-});
-
 // // with store.subscribe, we can watch over the store
 // // whenever there is a change, it will be called.
 // store.subscribe(() => {
 //   console.log(store.getState());
 // });
 
-// if we want to stop to watching store, 
-// assign the subscribe function to a variable
-// when you want to stop it, you need to call the variable as a function.
+// // if we want to stop to watching store, 
+// // assign the subscribe function to a variable
+// // when you want to stop it, you need to call the variable as a function.
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-// Actions - nothing more than an object that gets sent to the store
-// Actions allow us to change the store state.
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
+// // Actions - nothing more than an object that gets sent to the store
+// // Actions allow us to change the store state.
+// store.dispatch({
+//   type: 'INCREMENT',
+//   incrementBy: 5
+// });
+
+
 
 store.dispatch(incrementCount( {incrementBy: 5} ));
 
 store.dispatch(incrementCount());
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10
-});
+store.dispatch(resetCount());
+
+store.dispatch(decrementBy());
+
+store.dispatch(decrementBy( {decrementBy: 10} ));
 
 // // if you will activate this code,
 // // the subscribe will stop
 // unsubscribe();
 
-store.dispatch({
-  type: 'DECREMENT'
-});
-
-store.dispatch({
-  type: 'RESET'
-});
+store.dispatch(setCount({ count: 101 }));
