5884766b2c8878097c4e7633f1d2a5862913bafc
yektt <yektaturan.93@gmail.com>
2021-09-10 16:11:17 +0200

    object-rest-spread api has been downloaded

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -2,8 +2,31 @@ import { createStore, combineReducers } from "redux";
 // combineReducers will allow us to create multiple 
 // functions that define how our redux app changes.
 
+import uuid from 'uuid';
+
 // ADD_EXPENSE
+
+const addExpense = (
+  { description = '', 
+    note = '', 
+    amount = 0, 
+    createdAt = 0 
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
 // REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
 // EDIT_EXPENSE
 // SET_TEXT_FILTER
 // SORT_BY_DATE
@@ -17,6 +40,10 @@ const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense];
+    case 'REMOVE_EXPENSE':
+      return state.filter( ({id}) => id !== action.id );
     default:
       return state;
   }
@@ -37,7 +64,6 @@ const filterReducer = (state = filterReducerDefaultState, action) => {
   }
 };
 
-
 // Store creation
 
 const store = createStore(
@@ -47,7 +73,15 @@ const store = createStore(
   })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
+
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 const demoState = {
   expenses: [{
@@ -64,3 +98,12 @@ const demoState = {
     endDate: undefined
   }
 };
+
+const user = {
+  name: 'Jen',
+  age: 24
+};
+
+console.log({
+  ...user
+});
