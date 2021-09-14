ba38cf6b0796ed51d4032e808286a800e26c3dbf
yektt <yektaturan.93@gmail.com>
2021-09-14 17:42:23 +0200

    removing expense has been added via remove button

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
+import { editExpense, removeExpense } from "../actions/expenses";
 
-const ExpenseListItem = ({description, amount, createdAt}) => (
+const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
   <div>
     <h3>{description}</h3>
     <p>{amount} - {createdAt}</p>
+    <button onClick={(e) => {
+        dispatch(removeExpense({ id }));
+      }}
+    >
+      Remove
+    </button>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem); 
--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -6,7 +6,7 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
     case 'ADD_EXPENSE':
       return [...state, action.expense];
     case 'REMOVE_EXPENSE':
-      return state.filters( ({id}) => id !== action.id );
+      return state.filter( ({id}) => id !== action.id );
     case 'EDIT_EXPENSE':
       return state.map((expense) => {
         if (expense.id === action.id) {
