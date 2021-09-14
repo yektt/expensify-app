d39a6671945e1553ddcfb1d58d363e3c156cc074
yektt <yektaturan.93@gmail.com>
2021-09-14 15:50:36 +0200

    Higher order component has been added as an example - wrapping a component with another one

--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -1,2 +1,43 @@
-// Higher order component
+// Higher order component (HOC)
+// a component (HOC) that renders another component (regular one)
+// aim is reuse the code
+// render hijacking
+// prop manipulation
+// abstract state
 
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Info = (props) => (
+  <div>
+    <h1>Info</h1>
+    <p>The info is: {props.info}</p>
+  </div>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      {props.isAdmin && <p>This is private info. Please do not share!</p>}
+      <WrappedComponent {...props}/>
+    </div>
+  );
+};
+
+const requireAuthentication = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      {props.isAuthenticated ? (
+        <WrappedComponent {...props}/>
+      ) : (
+        <p>Please log in</p>
+      )}
+    </div>
+  );
+};
+
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+// ReactDOM.render(<AdminInfo isAdmin={false} info="There are the details" />, document.getElementById('app'));
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="There are the details" />, document.getElementById('app'));
