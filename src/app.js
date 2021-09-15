bfddc56534df6947a66c993eec346d7715a04237
yektt <yektaturan.93@gmail.com>
2021-09-15 11:41:50 +0200

    Dropdown menu has been added and made it work for filtering via date or amount

--- src/app.js
+++ src/app.js
@@ -11,17 +11,18 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('water'));
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
+// store.dispatch(setTextFilter('water'));
 
-setTimeout(() => {
-  store.dispatch(setTextFilter('bill'));
-}, 3000);
+// setTimeout(() => {
+//   store.dispatch(setTextFilter('bill'));
+// }, 3000);
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-//console.log(visibleExpenses);
+console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
