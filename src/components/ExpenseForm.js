6013a6c05d3f13a6f93e15e89168a366b62858f5
yektt <yektaturan.93@gmail.com>
2021-09-15 17:58:43 +0200

    Date has been displayed and made it available to chose previous dates

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,19 @@
 import React from "react";
+import moment from "moment";
+import { SingleDatePicker } from "react-dates";
+import 'react-dates/lib/css/_datepicker.css';
+
+// const date = new Date();
+const now = moment();
+console.log(now.format('MMM Do YYYY'));
 
 export default class ExpenseForm extends React.Component {
   state = {
     description: '',
     note: '',
-    amount: ''
+    amount: '',
+    createdAt: moment(),
+    calendarFocused: false
   };
   onDescriptionChange = (e) => {
     const description = e.target.value;
@@ -23,6 +32,13 @@ export default class ExpenseForm extends React.Component {
       this.setState(() => ({ amount }));
     }
   }
+  onDateChange = (createdAt) => {
+    this.setState(() => ({ createdAt }));
+  };
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({calendarFocused: focused }));
+  }
+
   render () {
     return (
       <div>
@@ -39,6 +55,14 @@ export default class ExpenseForm extends React.Component {
             placeholder='Amount'
             value={this.state.amount}
             onChange={this.onAmountChange}
+          />
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths= {1}
+            isOutsideRange={() => false }
           />
           <textarea
             placeholder='Add a note for your expense (optional)'
