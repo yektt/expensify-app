0eb1429873963552441bef4e73c5934a38de8fed
yektt <yektaturan.93@gmail.com>
2021-09-15 11:47:51 +0200

    ExpenseForm has been created to use adding expenses

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,8 +1,10 @@
 import React from "react";
+import ExpenseForm from "./ExpenseForm";
 
 const AddExpensePage = () => (
   <div>
-    This is from add expense component.
+    <h1>Add Expense </h1>
+    <ExpenseForm/>
   </div>
 );
 
